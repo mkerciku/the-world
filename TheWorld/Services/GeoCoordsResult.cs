af01919d384f03ca22c38833daff7ba886fac177
Mariglen Kerciku <mariokerciku91@gmail.com>
2017-10-11 10:02:22 +0200

    updated config.json with the bing key which will allow us to use the bing maps api.  also created getcoordsasync method to get the coords from the bing api and store it in our database

--- TheWorld/Services/GeoCoordsResult.cs
+++ TheWorld/Services/GeoCoordsResult.cs
@@ -2,5 +2,9 @@
 {
     public class GeoCoordsResult
     {
+        public bool Success { get; set; }
+        public string  Message { get; set; }
+        public double Longtitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
